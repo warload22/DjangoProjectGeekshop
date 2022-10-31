286446bf82c124fc790bdd62f935b811501e5049
warload22 <warload22@gmail.com>
2022-10-31 22:49:52 +0300

    1-step Blocks, URLS, %now%,

--- Project1/mainapp/views.py
+++ Project1/mainapp/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
-
+from django.utils.datetime_safe import datetime
+from datetime import datetime
 # Create your views here.
 
+# now = datetime.datetime.now()
 def index(request):
+    content = {
+        'title': 'Geegshop',
+        # 'time' : now
+    }
 
-    return render(request, 'mainapp/index.html')
+    return render(request, 'mainapp/index.html', content)
+
+
+
 
 def products(request):
+    categories = [
+        {'name': 'Новинки'},
+        {'name': 'Одежда'},
+        {'name': 'Обувь'},
+        {'name': 'Аксессуары'},
+        {'name': 'Подарки'},
+    ]
 
-    return render(request, 'mainapp/products.html')
+    content = {
+        'title': 'Geegshop - Каталог',
+        'categories': categories,
+        # 'time': now
+    }
+
+    return render(request, 'mainapp/products.html', content)
